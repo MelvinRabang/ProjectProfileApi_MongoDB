3bb35f378aa53bd65571a9befa2aa47e31cd12c9
MelvinRabang <mirabang_88@yahoo.com>
2018-07-18 07:22:43 +0800

    first commit

---
+++ src/main/java/com/doctorsenator/projectprofile/ProjectprofileApplication.java
@@ -0,0 +1,12 @@
+package com.doctorsenator.projectprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectprofileApplication.class, args);
+	}
+}
---
+++ src/test/java/com/doctorsenator/projectprofile/ProjectprofileApplicationTests.java
@@ -0,0 +1,16 @@
+package com.doctorsenator.projectprofile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectprofileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
